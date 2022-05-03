542df20d9cc247c01523492c2ef8b30da8b67b64
Lipon Roy <liponbdbd@gmail.com>
2022-05-03 08:35:26 +0600

    make product model

---
+++ model/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'please inter Product name'],
+	},
+	description: {
+		type: String,
+		required: [true, 'please inter Product Description'],
+	},
+	price: {
+		type: Number,
+		required: [true, 'please inter Price'],
+	},
+	quantity: {
+		type: Number,
+		required: [true, 'please inter Product quantity'],
+	},
+	supplier: {
+		type: String,
+		required: [true, 'please inter Product supplier name'],
+	},
+	imgUrl: {
+		type: String,
+		required: [true, 'please inter your image url'],
+	},
+});
+
+module.exports = mongoose.model('product', ProductSchema);
